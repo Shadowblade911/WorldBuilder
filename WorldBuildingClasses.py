17633d6a885d7dfb926d4a7c5394137883fb4f65
David Pogin <david.pogin@centriam.com>
2016-06-02 17:51:27 -0500

    Continuing to iterate the initial creation of the globe.

--- WorldBuildingClasses.py
+++ WorldBuildingClasses.py
@@ -34,26 +34,51 @@ class Node:
 
 class WorldSphere:
     def __init__(self):
+        #this creates a northern pole for us to use as an access point
         self.north_pole = Node( None, None, None, None, None, None, False)
 
-        one = Node(self.north_pole, None, None, None, None, False)
-        two = Node(self.north_pole, None, None, None, one, False)
-        three = Node(self.north_pole, None, None, None, two, False)
-        four = Node(self.north_pole, None, None, None, three, False)
-        five = Node(self.north_pole, None, None, None, one, False)
+        north_one = Node(self.north_pole, None, None, None, None, False)
+        north_two = Node(self.north_pole, north_one, None, None, None, False)
 
-        one.set_node_two(five)
-        one.set_node_five(two)
+        self.north_pole.set_node_two(north_two)
+        north_one.set_node_five(north_two)
 
-        one.set
-        two.set_node_two(five)
-        three.set_node_two(five)
-        four.set_node_two(five)
-        five.set_node_two(five)
+        north_three = Node(self.north_pole, north_two, None, None, None, False)
+        self.north_pole.set_node_three(north_three)
+        north_two.set_node_five(north_three)
 
-        self.north_pole.set_all_nodes(rnnw, rnne, rne, rnse,rnsw, rnw)
+        north_four = Node(self.north_pole, north_three, None, None, None, False)
 
+        self.north_pole.set_node_four(north_four)
+        north_three.set_node_five(north_four)
 
+        north_five = Node(self.north_pole, north_four, None, None, north_one, False)
 
+        self.north_pole.set_node_five(north_five)
 
+        #this creates the southern pole, using the same pattern as the north pole
         self.south_pole = Node( None, None, None, None, None, None, False)
+
+        south_one = Node(self.south_pole, None, None, None, None, False)
+        south_two = Node(self.south_pole, south_one, None, None, None, False)
+
+        self.south_pole.set_node_two(south_two)
+        south_one.set_node_five(south_two)
+
+        south_three = Node(self.south_pole, south_two, None, None, None, False)
+        self.south_pole.set_node_three(south_three)
+        south_two.set_node_five(south_three)
+
+        south_four = Node(self.south_pole, south_three, None, None, None, False)
+
+        self.south_pole.set_node_four(south_four)
+        south_three.set_node_five(south_four)
+
+        south_five = Node(self.south_pole, south_four, None, None, north_one, False)
+
+        self.south_pole.set_node_five(south_five)
+
+
+
+
+
